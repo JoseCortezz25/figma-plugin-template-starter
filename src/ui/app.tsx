c528c3ea76e3845ac7d1aeb560df1002ad626485
ajosecortes <alfonsochavarro@flare.com.co>
2025-08-06 16:16:16 -0500

    feat: update project structure with Tailwind CSS integration and ESLint configuration. Added new styles and components, removed unused files.

--- src/ui/app.tsx
+++ src/ui/app.tsx
@@ -1,35 +1,35 @@
-import { PLUGIN } from "@common/networkSides";
-import { UI_CHANNEL } from "@ui/app.network";
-import { Button } from "@ui/components/Button";
-import { Networker, NetworkError } from "monorepo-networker";
-import { useEffect, useState } from "react";
+import { PLUGIN } from '@common/networkSides';
+import { UI_CHANNEL } from '@ui/app.network';
+import { Button } from '@ui/components/ui/button';
+import { Networker, NetworkError } from 'monorepo-networker';
+import { useEffect, useState } from 'react';
 
-import figmaLogo from "@ui/assets/figma.png";
-import ReactLogo from "@ui/assets/react.svg?component";
-import viteLogo from "@ui/assets/vite.svg?url";
+import figmaLogo from '@ui/assets/figma.png';
+import ReactLogo from '@ui/assets/react.svg?component';
+import viteLogo from '@ui/assets/vite.svg?url';
 
-import "@ui/styles/main.scss";
+import '@ui/styles/main.css';
 
 function App() {
   const [count, setCount] = useState(0);
   const [pingCount, setPingCount] = useState(0);
 
   useEffect(() => {
-    UI_CHANNEL.subscribe("ping", () => {
-      setPingCount((cnt) => cnt + 1);
+    UI_CHANNEL.subscribe('ping', () => {
+      setPingCount(cnt => cnt + 1);
     });
   }, []);
 
   return (
     <div className="homepage">
-      <div>
-        <a href="https://www.figma.com" target="_blank">
+      <div className="mx-auto flex w-full items-center justify-center gap-2">
+        <a href="https://www.figma.com" target="_blank" rel="noreferrer">
           <img src={figmaLogo} className="logo figma" alt="Figma logo" />
         </a>
-        <a href="https://vitejs.dev" target="_blank">
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
           <img src={viteLogo} className="logo vite" alt="Vite logo" />
         </a>
-        <a href="https://reactjs.org" target="_blank">
+        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
           <ReactLogo className="logo react" title="React logo" />
         </a>
       </div>
@@ -37,13 +37,13 @@ function App() {
       <h1>Figma + Vite + React</h1>
 
       <div className="card">
-        <Button onClick={() => setCount((count) => count + 1)}>
+        <Button onClick={() => setCount(count => count + 1)}>
           count is {count}
         </Button>
         <Button
           onClick={async () => {
-            const response = await UI_CHANNEL.request(PLUGIN, "ping", []);
-            console.log("Response:", response);
+            const response = await UI_CHANNEL.request(PLUGIN, 'ping', []);
+            console.log('Response:', response);
           }}
           style={{ marginInlineStart: 10 }}
         >
@@ -51,8 +51,8 @@ function App() {
         </Button>
         <Button
           onClick={() => {
-            console.log("Create a rectangle, please!");
-            UI_CHANNEL.emit(PLUGIN, "createRect", [100, 100]);
+            console.log('Create a rectangle, please!');
+            UI_CHANNEL.emit(PLUGIN, 'createRect', [100, 100]);
           }}
           style={{ marginInlineStart: 10 }}
         >
@@ -63,10 +63,10 @@ function App() {
             try {
               const result = await UI_CHANNEL.request(
                 PLUGIN,
-                "exportSelection",
+                'exportSelection',
                 []
               );
-              console.log("Export: ", { result });
+              console.log('Export: ', { result });
             } catch (err) {
               if (err instanceof NetworkError) {
                 console.log("Couldn't export..", { message: err.message });
